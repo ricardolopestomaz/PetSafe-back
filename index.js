742cc26a92afb3e7691336ecffcb8d07496181f8
Anna Beatriz Moura de Oliveira <mouraanna53@gmail.com>
2026-09-01 18:43:07 -0300

    refactor: remove pasta view, adiciona routes e services

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const cors = require('cors');
-// const { listarPets, criarPet } = require('./app/controller/petController');
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// app.get('/pets', listarPets);
-// app.post('/pets', criarPet);
+
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`API rodando na porta ${PORT}`));
